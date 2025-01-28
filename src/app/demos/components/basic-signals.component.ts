64b266f551c0c5b4f6c832be0efd79197b8efd69
johnny339 <guzmanj774@gmail.com>
2025-01-28 14:39:07 -0500

    compute par

--- src/app/demos/components/basic-signals.component.ts
+++ src/app/demos/components/basic-signals.component.ts
@@ -7,19 +7,27 @@ import {
 import { GolfScoreComponent } from './golf-score.component';
 import { AnimalListComponent } from './animal-list.component';
 import { AddAnimalComponent } from './add-animal.component';
+import { SummaryComponent } from './summary.component';
 
 @Component({
   selector: 'app-basic-signals',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [GolfScoreComponent, AnimalListComponent, AddAnimalComponent],
+  imports: [
+    GolfScoreComponent,
+    AnimalListComponent,
+    AddAnimalComponent,
+    SummaryComponent,
+  ],
   template: `
-    <div>
-      <p>You have 99 Animals, and Your Score is 128</p>
-    </div>
+    <app-summary
+      [animalCount]="animalCount()"
+      [golfScore]="currentScore()"
+      message="Just a Hardcoded String"
+    />
 
     <div>
       <h2 class="text-2xl">Golfing!</h2>
-      <app-golf-score />
+      <app-golf-score (scoreChanged)="currentScore.set($event)" />
 
       <app-animal-list [list]="animals()" />
       <app-add-animal (animalAdded)="addAnimal($event)" />
@@ -29,8 +37,12 @@ import { AddAnimalComponent } from './add-animal.component';
 })
 export class BasicSignalsComponent {
   animals = signal(['Bird', 'Goat', 'Deer']);
+
+  currentScore = signal(0);
   // animalCount = computed(() => this.animals().length);
   addAnimal(animalName: string) {
     this.animals.update((currentAnimals) => [animalName, ...currentAnimals]);
   }
+
+  animalCount = computed(() => this.animals().length);
 }
--- src/app/demos/components/golf-score.component.ts
+++ src/app/demos/components/golf-score.component.ts
@@ -1,4 +1,10 @@
-import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  signal,
+  output,
+  computed,
+} from '@angular/core';
 
 @Component({
   selector: 'app-golf-score',
@@ -12,6 +18,21 @@ import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
 
     <div>
       <button (click)="addStroke()" class="btn btn-primary">Took A Shot</button>
+      @if (parMessage() !== '') {
+        <div class="alert alert-info">
+          @switch (parMessage()) {
+            @case ('at') {
+              <p>You are at par!</p>
+            }
+            @case ('close') {
+              <p>Getting close to par, good luck!</p>
+            }
+            @case ('over') {
+              <p>Uh, maybe rethink your hobby</p>
+            }
+          }
+        </div>
+      }
     </div>
   `,
   styles: ``,
@@ -19,9 +40,28 @@ import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
 export class GolfScoreComponent {
   currentScore = signal(0); // state
 
+  scoreChanged = output<number>();
+
   addStroke() {
     // this.currentScore.set(this.currentScore() + 1); // changeable
     this.currentScore.update((c) => c + 1);
     // hey, update the dom wherever the currentScore is shown now.
+    this.scoreChanged.emit(this.currentScore());
   }
+
+  parMessage = computed(() => {
+    const score = this.currentScore();
+    const par = 4;
+
+    if (score === par) {
+      return 'at';
+    }
+    if (score > par) {
+      return 'over';
+    }
+    if (par - score === 1) {
+      return 'close';
+    }
+    return '';
+  });
 }
